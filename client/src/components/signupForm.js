10836c1e2de6eb103b901ec3b5ce481d9dc93fa4
Ayele Lintamo <lashaaye@gmail.com>
2022-01-18 19:56:07 -0800

    setinup server.js

--- client/src/components/signupForm.js
+++ client/src/components/signupForm.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { createUser } from '../utils/API';
+import { useMutation } from "@apollo/client";
+
 import Auth from '../utils/auth';
+import { ADD_USER } from "../utils/mutations";
 
 const SignupForm = () => {
   // set initial form state
--- server/server.js
+++ server/server.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const path = require('path');
+const { ApollosServer } = require('apollo-server-express');
+
+const {typeDefs,resolveers} = require('./schemas');
+const { authMiddleware } =require('./utils/auth');
 const db = require('./config/connection');
-const routes = require('./routes');
+//const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+async function startApollo() {
+const server = new ApollosServer({
+  typeDefs,
+  resolvers,
+  
+  context: authMiddleware,
+});
+
+await server,start();
+
+server.applyMiddleware({ app });
+console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlpath}`)
+}
+
+startApollo();
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +35,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+//app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () =>  
+  console.log(`🌍 Now listening on localhost:${PORT}`));
 });
