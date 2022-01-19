6091a96b99046658a7ae09bf6ff88d91df03c5c4
Ayele Lintamo <lashaaye@gmail.com>
2022-01-18 20:55:59 -0800

    creating schemas filee

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,48 @@
+const { gql, } = require("apollo-server-express");
+
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        password: String 
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Auth {
+        token: ID
+        user: User
+    }
+
+    type Book {
+        bookId: String
+        authors: [String]
+        description :string
+        title: String
+        image: String
+    }
+
+    inpot BookSaved {
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+    }
+
+    
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        addUser(username: String!, email: String!, password: String!):  Auth
+        login(email: String!, password: String!): Auth
+
+        saveBook(input: BookSaved!): User
+        removeBook(bookId: String!): User
+    }
+    `;
+
+    module.exports = typeDefs;
